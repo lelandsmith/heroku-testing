7fb36f6353f4d730e3cc9a5bfe4b67dfafbc7a4e
Leland Smith <emailleland@gmail.com>
2014-04-19 14:44:55 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuTesting::Application.config.secret_key_base = '9d15407c9e196c9bb5f978a5b88fdfa67dc1045a35fc15334d0845efe368d8e14b6c915c55e953d5d4af1ba47d21fe92fd7e31b1d58aeaf9ce5d611fe270687c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HerokuTesting::Application.config.session_store :cookie_store, key: '_heroku-testing_session'
